9693a3404afc01a3a554e1426c5c4f84010fd6d9
Manohar-1305 <maddy98989@gmail.com>
2025-02-14 12:26:19 +0530

    Update 3.namespace_migration_controller.py

--- 3.namespace_migration_controller.py
+++ 3.namespace_migration_controller.py
@@ -12,7 +12,7 @@ api = client.CustomObjectsApi()
 core_api = client.CoreV1Api()
 apps_api = client.AppsV1Api()
 
-CRD_GROUP = "example.com"
+CRD_GROUP = "migrations.k8s.io"
 CRD_VERSION = "v1"
 CRD_PLURAL = "namespacemigrations"
 
